48c2a892042b3db218f4e47eb948deb64c37926c
Viraj Prabhu <prabhuviraj@gmail.com>
2016-04-23 11:37:45 +0530

    autocomplete fixed, bookmarking + keyboard shortcut implemented

--- scripts/popup.js
+++ scripts/popup.js
@@ -2,14 +2,19 @@
 // then initialize jquery-ui autocomplete
 function getFolderArr(callback) {
 	var folderArr = [];
+	// recursively collect bookmark folders, top down
 	function getBookmarks(bookmarks) {
 		bookmarks.forEach(function(bookmark) {
 			if (bookmark.children) {
-				folderArr.push(bookmark.title);
+				folderArr.push({
+					value: bookmark.title,
+					id: bookmark.id
+				});
 				getBookmarks(bookmark.children);
 			}
 		});
 	}
+	// chrome bookmarks api to get entire bookmarktreenode tree
 	chrome.bookmarks.getTree(function(bookmarks) {
 		getBookmarks(bookmarks);
 		callback(folderArr);
@@ -19,10 +24,33 @@ function getFolderArr(callback) {
 
 // initalize jquery-ui autocomplete
 function initAutocomplete(folderArr) {
-	$(".selector").autocomplete({
+	// folderArr.forEach(function(folder) {
+	// 	console.log(folder.id + " : " + folder.value);
+	// });
+	var isSelected = false;
+	$("#tags").autocomplete({
 		minLength: 0,
-		source: folderArr
-	});	
+		source: folderArr,
+		select: function(event, ui) {
+			isSelected = true;
+			var selectedFolderValue = $(event.target).val(ui.item.value)[0].value;
+			var selectedFolderId = $.grep(folderArr, function(e) {
+				return e.value === selectedFolderValue;
+			})[0].id;
+			// console.log(selectedFolderId + " : " + selectedFolderValue);
+			chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+				chrome.bookmarks.create({
+					parentId: selectedFolderId,
+					title: tabs[0].title, 
+					url: tabs[0].url 
+				});
+				chrome.browserAction.setIcon({path: icon});
+			})
+			
+		}
+	});
 }
 
+// pass autocomplete init as callback
 getFolderArr(initAutocomplete);
+
